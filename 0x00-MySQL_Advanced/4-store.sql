90dda0a123526dbaa5d7c10c19a37456f069f986
LabyKreative <labykreative@yahoo.com>
2023-10-12 19:47:49 +0000

    Buy! Buygit commit -m Buy! Buygit add . Buygit add .! Buygit commit -m Buy! Buygit add . Buygit add .!!

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,12 @@
 -- This SQL script creates a trigger to decrease item quantity after adding a new order.
 DROP TRIGGER IF EXISTS after_insert_order;
-DELIMITER //
+DELIMITER $$
 CREATE TRIGGER after_insert_order
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-END //
+    SET quantity = quantity - NEW.number
+    WHERE item_id = NEW.item_name;
+END $$
 DELIMITER ;
