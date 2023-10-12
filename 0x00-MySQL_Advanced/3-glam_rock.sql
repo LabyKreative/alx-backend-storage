a963999c1539dd0e66e595489ee7aed3ada6d3b9
LabyKreative <labykreative@yahoo.com>
2023-10-12 19:34:25 +0000

    Old School Band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- This SQL script lists all bands with Glam rock as their main style, ranked by their longevity.
-SELECT band_name, 
-       YEAR(2022) - CAST(SUBSTRING_INDEX(lifespan, '-', 1) AS INT) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
