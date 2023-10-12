c7de695625b2d5f5ed8604c8e00ad8b49abffe42
LabyKreative <labykreative@yahoo.com>
2023-10-12 19:58:54 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- This SQL script creates a trigger to reset valid_email when the email is updated.
+DROP TRIGGER IF EXISTS before_update_email;
+DELIMITER $$
+CREATE TRIGGER before_update_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
+END $$
+DELIMITER ;
