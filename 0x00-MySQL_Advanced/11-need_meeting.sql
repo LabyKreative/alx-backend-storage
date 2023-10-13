ab8b26c6025b32cb27094284aa78cac96ca4a9e9
LabyKreative <labykreative@yahoo.com>
2023-10-13 01:22:15 +0000

    No table for a meeting!

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,6 @@
 -- This SQL script creates a view need_meeting that lists students who need a meeting.
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT id, name, score
+SELECT name
 FROM students
 WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
