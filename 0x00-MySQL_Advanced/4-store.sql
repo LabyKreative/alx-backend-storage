48c7a5523461cfb5d2626b3e46adf72554d641d7
LabyKreative <labykreative@yahoo.com>
2023-10-13 00:36:17 +0000

    Buy, buy, buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- This SQL script creates a trigger to decrease item quantity after adding a new order.
 DROP TRIGGER IF EXISTS after_insert_order;
-DELIMITER $$
+DELIMITER //
 CREATE TRIGGER after_insert_order
 AFTER INSERT ON orders
 FOR EACH ROW
@@ -8,5 +8,5 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END $$
+END //
 DELIMITER ;
