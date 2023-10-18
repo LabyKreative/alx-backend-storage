cd3f4a371e574cb98df50e6b6a5a22a77b1068fb
LabyKreative <labykreative@yahoo.com>
2023-10-17 21:28:14 -0700

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""a function that..."""
+
+
+def top_students(mongo_collection):
+    """returns all all students sorted by average score."""
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
