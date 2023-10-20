4a6c3563a0cdacb52e611c0a68fbbc4815e4564e
LabyKreative <labykreative@yahoo.com>
2023-10-20 02:23:16 +0000

    Implementing an expiring web cache & trackergit add

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,43 +2,20 @@
 """get_page function using the requests module to fetch the
 content of a URL and cache it using a decorator.
 """
-import requests
-import time
 import redis
-from functools import lru_cache
+import requests
+r = redis.Redis()
+count = 0
 
-# Initialize a Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-# Define a decorator to cache the result with a 10-second expiration time
-def cache_result(expiration=10):
-    def decorator(func):
-        def wrapper(url):
-            # Check if the result is already cached in Redis
-            cached_data = redis_client.get(url)
-            if cached_data:
-                return cached_data.decode('utf-8')
+def get_page(url: str) -> str:
+    """Define the key for caching."""
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
 
-            # If not cached, fetch the data and cache it in Redis
-            data = func(url)
-            redis_client.setex(url, expiration, data)
-            return data
-
-        return wrapper
-
-    return decorator
-
-# Apply the cache_result decorator to the get_page function
-@cache_result()
-def get_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return f"Failed to fetch page from {url}"
 
 if __name__ == "__main__":
-    # Test the get_page function
-    url = "http://slowwly.robertomurray.co.uk"
-    content = get_page(url)
-    print(content)
+    get_page("http://slowwly.robertomurray.co.uk")
